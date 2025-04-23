a348d41e2fb061ca171caa077f838af27c2e1178
gold496 <aldainewallace@gmail.com>
2025-04-22 20:21:46 -0500

    added the hover square effect

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-let size =Number( prompt("Enter gris size:"));
+
 let container = document.getElementById("container");
 let row;
 let column;
 
-for(let i=0;i<size;i++){
+function grid(size){
+    for(let i=0;i<size;i++){
 
-    column=  document.createElement("div");
-    column.classList.add("column");
-
-    for(let j=0;j<size;j++){
-    row = document.createElement("div");
-    row.classList.add("row");
-    column.appendChild(row);
+        column=  document.createElement("div");
+        column.classList.add("column");
+    
+        for(let j=0;j<size;j++){
+        row = document.createElement("div");
+        row.classList.add("row");
+        column.appendChild(row);
+        }
+        container.appendChild(column);
     }
-    container.appendChild(column);
 }
+
+grid(16);
+
+
+const hovers = document.querySelectorAll(".row");
+hovers.forEach( hover =>{
+    hover.addEventListener("mouseover",()=>{
+        hover.style.backgroundColor="black";
+    })
+})
+
+function newgrid(){
+    location.reload(); // Reloads the current page
+    let n;
+    do{
+        n =Number(prompt("Enter gris size:"));
+
+    }while(n<0 && n>100);
+    grid(n);
+}
+document.querySelector("button").addEventListener("click",newgrid);
